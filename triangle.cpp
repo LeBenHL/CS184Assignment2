55da0e78c23f1d3de7a2d71327ba12e39b42b0e7
Lichen <thisislichen@berkeley.edu>
2013-10-11 21:47:00 -0700

    refactored triangle w/ comments

--- triangle.cpp
+++ triangle.cpp
@@ -30,29 +30,7 @@ bool Triangle::hit(Ray* ray, Record* record){
 	float k = this->a->y - ray->position->y;
 	float l = this->a->z - ray->position->z;
 	
-	/*Matrix3f A;
-	Vector3f b;
-
-	float a1 = this->a->x - this->b->x;
-	float a2 = this->a->x - this->c->x;
-	float a3 = ray->direction->x;
-	float a4 = this->a->y - this->b->y;
-	float a5 = this->a->y - this->c->y;
-	float a6 = ray->direction->y;
-	float a7 = this->a->z - this->b->z;
-	float a8 = this->a->z - this->c->z;
-	float a9 = ray->direction->z;
-
-	A << a1,a2,a3,  a4,a5,a6,  a7,a8,a9;
-
-	float b1 = this->a->x - ray->position->x;
-	float b2 = this->a->y - ray->position->y;
-	float b3 = this->a->z - ray->position->z;
-
-	b << b1, b2, b3;
-
-	Vector3f x = A.colPivHouseholderQr().solve(b);*/
-
+	//reuse floats to reduce number of operations
 	float ei_minus_hf = e*i-h*f;
 	float gf_minus_di = g*f-d*i;
 	float dh_minus_eg = d*h-e*g;
@@ -60,6 +38,7 @@ bool Triangle::hit(Ray* ray, Record* record){
 	float jc_minus_al = j*c-a*l;
 	float bl_minus_kc = b*l-k*c;
 
+	//determinant of A
 	float m = a*ei_minus_hf + b*gf_minus_di + c*dh_minus_eg;
 
 	float t = f*ak_minus_jb + e*jc_minus_al + d*bl_minus_kc;
